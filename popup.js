861280bc3d215134db83bae8086d3d188fdcd9cc
Marton Tasnadi <marton.tasnadi@gmail.com>
2023-01-03 21:39:53 +0100

    transform xml articles to json

--- popup.js
+++ popup.js
@@ -8,7 +8,27 @@ async function getArticles() {
   return response;
 }
 
+function transformArticlesToJson(articles) {
+  const items = articles.querySelectorAll('rss channel item');
+  let results = [];
+
+  items.forEach(item => {
+    let date = item.querySelector('pubDate').textContent;
+    date = new Date(date).toLocaleDateString('hu-HU');
+
+    results.push({
+      title: item.querySelector('title').textContent,
+      link: item.querySelector('link').textContent,
+      date: date,
+      description: item.querySelector('description').textContent,
+    });
+  });
+
+  return results;
+}
+
 (async () => {
-  const articles = await getArticles();
+  let articles = await getArticles();
+  articles = transformArticlesToJson(articles);
   console.log(articles);
 })();
